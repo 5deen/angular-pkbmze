b485e331d0587aaf68bad1603cc11e571f0a38b2
5deen <83090020+5deen@users.noreply.github.com>
2023-02-04 21:07:15 +0100

    2 files modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main/main.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [CommonModule, BrowserModule],
-  declarations: [MainComponent],
+  declarations: [AppComponent],
   providers: [],
-  bootstrap: [MainComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
