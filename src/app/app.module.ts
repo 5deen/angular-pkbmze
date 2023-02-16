45a98d6d3716a3abc5e3e13db54fc9f1809fb47d
5deen <83090020+5deen@users.noreply.github.com>
2023-02-16 18:00:49 +0100

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MattoolbarComponent } from './mattoolbar/mattoolbar.component';
 
 @NgModule({
-  imports: [CommonModule, BrowserModule],
-  declarations: [AppComponent],
+  imports: [CommonModule, BrowserModule, MatToolbarModule, MatIconModule],
+  declarations: [AppComponent, MattoolbarComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
