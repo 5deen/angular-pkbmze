4a56470cd13828655a4cef6f8e311018d40142ae
5deen <83090020+5deen@users.noreply.github.com>
2023-02-16 18:34:01 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatIconRegistry} from '@angular/material/icon';
 import {MattoolbarComponent } from './mattoolbar/mattoolbar.component';
 
 @NgModule({
   imports: [CommonModule, BrowserModule, MatToolbarModule, MatIconModule],
   declarations: [AppComponent, MattoolbarComponent],
-  providers: [],
+  providers: [MatIconRegistry],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
